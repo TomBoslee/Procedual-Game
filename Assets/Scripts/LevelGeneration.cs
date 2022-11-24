a4892f3f2c196f44d70e4579c1f4d5e6e1477a78
TomBoslee <thomascarruthers02@gmail.com>
2022-11-24 00:42:55 +0000

    Added decoder
    Changed fundamental way of generating objects

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -14,6 +14,8 @@ public class LevelGeneration : MonoBehaviour
     public Transform Spike;
     public int ChunkMax;
 
+    private string BackwardsSpike = "000030010";
+
     private void Awake()
     {
         Terrain.AddComponent<TilemapCollider2D>();
@@ -21,16 +23,13 @@ public class LevelGeneration : MonoBehaviour
     }
     void Start()
     {
-        int index = -8;
-        for (int i = 0; i < ChunkMax; i++)
+        for (int i = -8; i < 3 * ChunkMax; i = i + 3)
         {
-            GenerateChunk(index);
-            index = index + 4;
+            GenerateChunk(i);
         }
         GeneratePlayer();
-        //GenerateSpike(1,12.5f,1.5f);
-        //GenerateStairs(0,16, 1);
-        GenerateSpikeDuck(16, 1);
+        Decoder(BackwardsSpike, 16, 1);
+        // GenerateSpikeDuck(16, 1);
 
     }
 
@@ -49,12 +48,44 @@ public class LevelGeneration : MonoBehaviour
 
     }
 
+    private void Decoder(string code, int posX,int posY)
+    {
+        int X = posX;
+        int Y = posY;
+        for (int n = 0; n < code.Length ; n++ )
+        {
+            if (code[n] == '1')
+            {
+                GenerateTile(X, Y);
+            }
+            else if (code[n] == '2')
+            {
+                GenerateSpike(X, Y);
+            }
+            else if (code[n] == '3')
+            {
+                Transform spike =  GenerateSpike(X,Y);
+                RotateSpike(spike);
+            }
+
+            X++;
+            if (X == posX + 3)
+            {
+                X = posX;
+                Y++;
+            }
+
+        }
+        
+
+    }
+
     private void GenerateChunk(int x)
     {
 
-        for (int y = 0; y > -4; y = y-1)
+        for (int y = 0; y > -3; y = y-1)
         {
-            for (int z = x; z < x + 4; z++)
+            for (int z = x; z < x + 3; z++)
             {
                 Terrain.SetTile(new Vector3Int(z, y, 0), tileBases[4]);
             }
@@ -63,12 +94,16 @@ public class LevelGeneration : MonoBehaviour
         
         
     }
+    private void GenerateTile(int posX, int PosY)
+    {
+        Terrain.SetTile(new Vector3Int(posX, PosY, 0 ), tileBases[4]);
+    }
 
     private Transform GenerateSpike(float Posx, float Posy)
     {
         Transform spike = Instantiate(Spike);
         spike.name = "Spike";
-        spike.position = new Vector2(Posx, Posy);
+        spike.position = new Vector2(Posx + 0.5f, Posy + 0.5f);
         return spike;
     }
 
