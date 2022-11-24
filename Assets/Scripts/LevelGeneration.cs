1e45ed28fa678b2058d5a019c9830bf0b0a741f6
TomBoslee <thomascarruthers02@gmail.com>
2022-11-24 00:01:20 +0000

    Spike Generation
    Changed how spike are generated

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -28,7 +28,10 @@ public class LevelGeneration : MonoBehaviour
             index = index + 4;
         }
         GeneratePlayer();
-        GenerateSpike(1);
+        //GenerateSpike(1,12.5f,1.5f);
+        //GenerateStairs(0,16, 1);
+        GenerateSpikeDuck(16, 1);
+
     }
 
     // Update is called once per frame
@@ -49,7 +52,7 @@ public class LevelGeneration : MonoBehaviour
     private void GenerateChunk(int x)
     {
 
-        for (int y = -2; y > -6; y = y-1)
+        for (int y = 0; y > -4; y = y-1)
         {
             for (int z = x; z < x + 4; z++)
             {
@@ -61,21 +64,38 @@ public class LevelGeneration : MonoBehaviour
         
     }
 
-    private void GenerateSpike(int Amount)
+    private Transform GenerateSpike(float Posx, float Posy)
     {
-        for(int i = 0; i < Amount; i++)
-        {
         Transform spike = Instantiate(Spike);
-        spike.name = "Spike" + i;
-        spike.transform.position = new Vector2(12.5f + i, -0.5f);
-        }
+        spike.name = "Spike";
+        spike.position = new Vector2(Posx, Posy);
+        return spike;
     }
 
-    private void GeneratePlayer()
+    private void GenerateStairs(int Amount, int Posx, int Posy)
+    {
+        Terrain.SetTile(new Vector3Int(Posx, Posy, 0), tileBases[4]);
+        Terrain.SetTile(new Vector3Int(Posx + 5, Posy, 0), tileBases[4]);
+        Terrain.SetTile(new Vector3Int(Posx + 5, Posy + 1,0), tileBases[4]);
+    }
+
+    private void GenerateSpikeDuck(int Posx, int Posy)
+    {
+        Terrain.SetTile(new Vector3Int(Posx, Posy + 2, 0), tileBases[4]);
+        Transform spike = GenerateSpike(Posx + 0.5f, Posy + 1.5f);
+        RotateSpike(spike);
+    }
+
+    private void RotateSpike(Transform Spike)
+    {
+        Spike.rotation = Quaternion.Euler(0, 0, 180);
+    }
+        
+        private void GeneratePlayer()
     {
         Transform Player = Instantiate(player);
         Player.name = "Player";
-        Player.transform.position = WorldInfo.GetSpawn();
+        Player.position = WorldInfo.GetSpawn();
         Player.AddComponent<BoxCollider2D>();
     }
 }
