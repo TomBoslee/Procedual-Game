3740a798b1cd9737bff4a2c0ab309f6e89df31e5
TomBoslee <thomascarruthers02@gmail.com>
2023-01-23 21:11:05 +0000

    Settings
    added settings to the game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using Unity.UI;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer masterMixer;
+
+    public Dropdown ResolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        ResolutionDropdown.ClearOptions();
+
+        List<string> Options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i  < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            Options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height) { 
+                currentResolutionIndex= i;
+            }
+        }
+
+        ResolutionDropdown.AddOptions(Options);
+        ResolutionDropdown.value = currentResolutionIndex;
+        ResolutionDropdown.RefreshShownValue();
+
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
@@ -13,4 +47,22 @@ public class MainMenu : MonoBehaviour
     public void QuitGame() { 
         Application.Quit();
     }
+
+    public void SetVolume(float volume) {
+        masterMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex) { 
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen) { 
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex) { 
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height, Screen.fullScreen);
+    }
+
 }
